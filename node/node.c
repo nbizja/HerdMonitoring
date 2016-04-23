c87a213b9d542a925e3bc8cec71e77c403f30c8f
Nejc Bizjak <nb7232@student.uni-lj.si>
2016-04-23 16:06:54 +0200

    Added gitignore file and fixed MakeFile CONTIKI path.

--- node/node.c
+++ node/node.c
@@ -12,16 +12,44 @@
 #include <stdlib.h>
 
 #define TMP102_READ_INTERVAL (CLOCK_SECOND)  // Poll the sensor every second
+#define NUMBER_OF_COWS 15 //Number of cows 
 
 PROCESS (herd_monitor_node, "Herd monitor - node");
 AUTOSTART_PROCESSES (&herd_monitor_node);
 
+static void
+broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
+{
+	int cow_id = from->u8[1];
+  printf("broadcast message received from cow %d: '%s'\n",
+         from->u8[1], (char *)packetbuf_dataptr());
+}
+
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+static struct broadcast_conn broadcast;
+
+static int neighbour_list[NUMBER_OF_COWS - 1];
+
 PROCESS_THREAD (herd_monitor_node, ev, data)
 {
+  static struct etimer et;
+
+  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+	
 	PROCESS_BEGIN();
-	{
-		
-	}
+
+  broadcast_open(&broadcast, 129, &broadcast_call);
+  while(1) {
+    /* Delay 2-4 seconds */
+    etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+    packetbuf_copyfrom("Hello", 6);
+    broadcast_send(&broadcast);
+    printf("broadcast message sent\n");
+  }
+
 
   PROCESS_END ();
 }		
